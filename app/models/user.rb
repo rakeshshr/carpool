642784fa14724fb55e05c93e339a27a118f0a19c
rakesh shrestha <shrestha.rakesh41@gmail.com>
2015-09-19 17:58:59 -0700

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+	geocoded_by :address   # can also be an IP address
+	after_validation :geocode          # auto-fetch coordinates
+
+
+
+end
+
